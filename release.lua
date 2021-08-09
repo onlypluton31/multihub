2f6eab95fcdb4a02d30eef321d21c77f2a63e778
onlypluton31 <88627571+onlypluton31@users.noreply.github.com>
2021-08-09 12:59:29 +0300

    Create release.lua

---
+++ release.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/onlypluton31/multihub/main/calanin/annesini/gotunden/sike/sike/sikiyorum/anladinmi/pic.lua", true))()
+--Calanin annesini gotunden sikiyorum
+--Orosbu Cocukları
